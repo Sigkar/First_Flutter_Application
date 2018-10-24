c7f6255563213a8c98a5b20df4ef0eb62d3d8502
sigkar <sigkar@github.com>
2018-10-24 00:38:55 -0400

    Added navigation and tabs - tabs also have icons. Tabs also have text specific to each page. Added some notes about how these work in comments

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -1,36 +1,60 @@
 import 'package:flutter/material.dart';
 import './discovery.dart';
 
-class ProductAdminPage extends StatelessWidget{
+import '../products/product_create.dart';
+import '../products/product_list.dart';
 
-@override
-  Widget build(BuildContext context){
-    return Scaffold(
-      drawer: Drawer(
-        child: Column(
+class ProductAdminPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      //Defines the length of the tabs
+      length: 2,
+      child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text("Main Menu"),
+              ),
+              ListTile(
+                title: Text("Discovery Feed"),
+                onTap: () {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => DiscoveryPage()));
+                }, // onTap
+              ), // ListTile
+            ], // Column Children Widget
+          ), // Column
+        ), // Drawer
+        appBar: AppBar(
+          title: Text("Product Admin Page"),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.create),
+                text: "Create Product"
+              ), // Tab
+              Tab(
+                icon: Icon(Icons.list),
+                text: "My Products",
+              ), // Tab
+            ], // Tabs
+          ), // Tab Bar
+        ), //App Bar
+        // Body pulls product manager.
+        body: TabBarView(
           children: <Widget>[
-            AppBar(
-              automaticallyImplyLeading: false,
-              title: Text("Main Menu"),
-            ),
-            ListTile(
-              title: Text("Discovery Feed"),
-              onTap: () {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => DiscoveryPage()));
-              }, // onTap
-            ), // ListTile
-          ], // Column Children Widget
-        ), // Column
-      ), // Drawer
-      appBar: AppBar(
-        title: Text("Product Admin Page"),
-      ), //App Bar
-      // Body pulls product manager.
-      body: Text("Hello World"),
-    ); // Scaffold
-  }
+            // Has to equal the number of tabs
+            ProductCreatePage(),
+            ProductListPage(),
+          ],
+        ),
 
+      ), // Scaffold
+    );
+  }
 }
